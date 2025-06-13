3238586d41795e24b03e733dbb85fb52c9881602
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-06-13 09:26:16 +0500

    Added the Right Sidebar Basic UI

--- src/components/shared/RightSidebar.jsx
+++ src/components/shared/RightSidebar.jsx
@@ -1,7 +1,46 @@
 // <= IMPORTS =>
+import { useSelector } from "react-redux";
+import SuggestedUsers from "../user/SuggestedUsers";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+  // CURRENT USER CREDENTIALS
+  const { user } = useSelector((store) => store.auth);
+  // AVATAR FALLBACK MANAGEMENT
+  const fullName = user?.fullName || "";
+  // DERIVING PARTS OF THE FULLNAME
+  const fullNameParts = fullName.split(" ").filter(Boolean);
+  // GETTING INITIALS OF THE FULLNAME
+  const fullNameInitials =
+    fullNameParts.length > 1
+      ? (
+          fullNameParts[0][0] + fullNameParts[fullNameParts.length - 1][0]
+        ).toUpperCase()
+      : fullName.slice(0, 2).toUpperCase();
+  return (
+    // RIGHT SIDEBAR MAIN WRAPPER
+    <div className="flex flex-col gap-6 items-start justify-start pr-36">
+      {/* AVATAR & USERNAME */}
+      <div className="flex items-center gap-3">
+        {/* AVATAR */}
+        <Avatar
+          className={`w-11 h-11 cursor-pointer ${
+            user.profilePhoto === "" ? "bg-gray-300" : "bg-none"
+          } `}
+        >
+          <AvatarImage src={user.profilePhoto} alt={user.fullName} />
+          <AvatarFallback>{fullNameInitials}</AvatarFallback>
+        </Avatar>
+        {/* USERNAME */}
+        <div className="flex flex-col">
+          <span className="font-semibold text-[0.9rem]">{user?.username}</span>
+          <span className="text-gray-500 text-sm">{user?.fullName}</span>
+        </div>
+      </div>
+      {/* SUGGESTED USERS */}
+      <SuggestedUsers />
+    </div>
+  );
 };
 
 export default RightSidebar;
---
+++ src/components/user/SuggestedUsers.jsx
@@ -0,0 +1,15 @@
+// <= IMPORTS =>
+
+const SuggestedUsers = () => {
+  return (
+    // SUGGESTED USERS MAIN WRAPPER
+    <div>
+      {/* HEADING */}
+      <div className="text-gray-500 font-semibold text-[0.9rem]">
+        <h1>Suggested for you</h1>
+      </div>
+    </div>
+  );
+};
+
+export default SuggestedUsers;
