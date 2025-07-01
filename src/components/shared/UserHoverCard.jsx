488d09d5d558cdb378ca985169a9e3a652c923d2
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-07-01 16:06:11 +0500

    Extended the Hover Card Features

--- src/components/shared/UserHoverCard.jsx
+++ src/components/shared/UserHoverCard.jsx
@@ -1,12 +1,23 @@
 // <= IMPORTS =>
-import { useState } from "react";
+import { toast } from "sonner";
 import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+import { setUser } from "@/redux/authSlice";
 import axiosClient from "@/utils/axiosClient";
 import { useNavigate } from "react-router-dom";
+import { setChatUser } from "@/redux/chatSlice";
 import { useQuery } from "@tanstack/react-query";
+import { useDispatch, useSelector } from "react-redux";
 import { getFullNameInitials } from "@/utils/getFullNameInitials";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { Image, Loader2, MessageCircle, UserPlus } from "lucide-react";
+import {
+  Edit,
+  Image,
+  Loader2,
+  MessageCircle,
+  UserMinus,
+  UserPlus,
+} from "lucide-react";
 import {
   HoverCard,
   HoverCardContent,
@@ -16,8 +27,24 @@ import {
 const UserHoverCard = ({ user, children }) => {
   // NAVIGATION
   const navigate = useNavigate();
+  // DISPATCH
+  const dispatch = useDispatch();
+  // CURRENT USER CREDENTIALS
+  const { user: currentUser } = useSelector((store) => store.auth);
   // HOVER CARD OPEN STATE
   const [open, setOpen] = useState(false);
+  // FOLLOWING STATE MANAGEMENT
+  const [isFollowing, setIsFollowing] = useState(false);
+  // FOLLOWING LOADING STATE
+  const [followLoading, setFollowLoading] = useState(false);
+  // SYNCING THE INITIAL FOLLOWING STATE
+  useEffect(() => {
+    if (currentUser?.following) {
+      setIsFollowing(currentUser?.following?.includes(user?._id));
+    }
+  }, [currentUser, user]);
+  // OTHER USER FOLLOWING STATUS
+  const isFollowingMe = user?.following?.includes(currentUser._id);
   // AVATAR FALLBACK MANAGEMENT
   const fullNameInitials = user?.fullName
     ? getFullNameInitials(user?.fullName)
@@ -32,6 +59,47 @@ const UserHoverCard = ({ user, children }) => {
     enabled: open,
     staleTime: 5 * 60 * 1000,
   });
+  // FOLLOW OR UNFOLLOW HANDLER
+  const followOrUnfollowHandler = async () => {
+    // LOADING STATE
+    setFollowLoading(true);
+    // SNAPSHOT OF ORIGINAL FOLLOWING LIST
+    const originalFollowing = currentUser?.following
+      ? [...currentUser.following]
+      : [];
+    // OPTIMISTICALLY TOGGLING FOLLOWING STATE
+    const newFollowing = isFollowing
+      ? originalFollowing.filter((id) => id !== user?._id)
+      : [user?._id, ...originalFollowing];
+    // DISPATCHING UPDATED FOLLOWING IN THE AUTH USER
+    dispatch(setUser({ ...currentUser, following: newFollowing }));
+    // UPDATING THE FOLLOWING STATE
+    setIsFollowing(!isFollowing);
+    // MAKING REQUEST
+    try {
+      const response = await axiosClient.get(
+        `/user/followOrUnfollow/${user?._id}`
+      );
+      // IF RESPONSE SUCCESS
+      if (response.data.success) {
+        // TOASTING SUCCESS MESSAGE
+        toast.success(response.data.message);
+      }
+    } catch (error) {
+      // REVERTING CHANGES ON ERROR
+      dispatch(setUser({ ...currentUser, following: originalFollowing }));
+      setIsFollowing(isFollowing);
+      // LOGGING ERROR MESSAGE
+      console.error("Failed to Perform Action!", error);
+      // TOASTING ERROR MESSAGE
+      toast.error(
+        error?.response?.data?.message || "Failed to Perform Action!"
+      );
+    } finally {
+      // LOADING STATE
+      setFollowLoading(false);
+    }
+  };
   console.log(error);
   return (
     <HoverCard open={open} onOpenChange={setOpen}>
@@ -111,34 +179,59 @@ const UserHoverCard = ({ user, children }) => {
               </div>
             )}
             {/* IF NO POSTS */}
-            {!isLoading &&
-              error?.response?.data?.message === "No Posts Found!" && (
-                <div className="w-full h-[9rem] flex flex-col gap-3 items-center justify-center bg-gray-100">
-                  <Image size={40} className="text-red-500" />
-                  <span className="text-gray-500 font-semibold text-[0.9rem]">
-                    No posts available
-                  </span>
-                </div>
-              )}
+            {!isLoading && data?.length === 0 && (
+              <div className="w-full h-[9rem] flex flex-col gap-3 items-center justify-center bg-gray-100">
+                <Image size={40} className="text-red-500" />
+                <span className="text-gray-500 font-semibold text-[0.9rem]">
+                  No posts available
+                </span>
+              </div>
+            )}
           </div>
           {/* FOLLOW & MESSAGE BUTTON */}
           <div className="w-full pt-2 pb-4 flex items-center justify-center gap-3 px-5">
-            {/* FOLLOW BUTTON */}
-            <Button
-              type="button"
-              className="bg-sky-400 hover:bg-sky-500 font-medium focus:outline-none outline-none border-none text-white text-[1rem] cursor-pointer w-1/2"
-            >
-              <UserPlus size={50} />
-              Follow
-            </Button>
-            {/* MESSAGE BUTTON */}
-            <Button
-              type="button"
-              className="bg-sky-400 hover:bg-sky-500 font-medium focus:outline-none outline-none border-none text-white text-[1rem] cursor-pointer w-1/2"
-            >
-              <MessageCircle size={50} />
-              Message
-            </Button>
+            {/* IF THE USER IS THE CURRENT USER */}
+            {user?._id === currentUser._id && (
+              <Button
+                type="button"
+                onClick={() => navigate(`/home/account/edit`)}
+                className="w-full bg-sky-400 hover:bg-sky-500 font-medium focus:outline-none outline-none border-none text-white text-[1rem] cursor-pointer"
+              >
+                <Edit size={50} />
+                Edit Profile
+              </Button>
+            )}
+            {/* IF THE USER IS NOT THE CURRENT USER */}
+            {user?._id !== currentUser._id && (
+              <>
+                {/* FOLLOW BUTTON */}
+                <Button
+                  type="button"
+                  onClick={followOrUnfollowHandler}
+                  disabled={followLoading}
+                  className="bg-sky-400 hover:bg-sky-500 font-medium focus:outline-none outline-none border-none text-white text-[1rem] cursor-pointer w-1/2"
+                >
+                  {isFollowing ? <UserMinus /> : <UserPlus />}
+                  {isFollowing
+                    ? "Unfollow"
+                    : isFollowingMe
+                    ? "Follow Back"
+                    : "Follow"}
+                </Button>
+                {/* MESSAGE BUTTON */}
+                <Button
+                  type="button"
+                  onClick={() => {
+                    navigate("/home/chat");
+                    dispatch(setChatUser(user));
+                  }}
+                  className="bg-sky-400 hover:bg-sky-500 font-medium focus:outline-none outline-none border-none text-white text-[1rem] cursor-pointer w-1/2"
+                >
+                  <MessageCircle size={50} />
+                  Message
+                </Button>
+              </>
+            )}
           </div>
         </div>
       </HoverCardContent>
