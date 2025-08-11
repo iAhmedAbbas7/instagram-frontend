e5d1b39850e1a225f5652f2343d26afad0443164
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-08-11 23:30:04 +0500

    Attached the Story Tray Component in the Feed Component

--- src/components/user/Feed.jsx
+++ src/components/user/Feed.jsx
@@ -1,10 +1,12 @@
 // <= IMPORTS =>
 import Posts from "./Posts";
+import StoryTray from "../story/StoryTray";
 
 const Feed = () => {
   return (
     // FEED MAIN WRAPPER
     <div className="flex flex-col flex-1 items-center justify-center max-[768px]:pl-[0] max-[768px]:px-2 max-[768px]:pt-[75px] max-[768px]:pb-[35px] max-[1200px]:pl-[70px] pl-[250px]">
+      <StoryTray />
       <Posts />
     </div>
   );
