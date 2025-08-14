f6a926fa5bd4d0b66c26d6e80ac066c9d6fe3fdd
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-08-14 23:08:46 +0500

    Implemented the Story Carousel Component in the Story Tray with Options

--- src/components/testing/StoryTrayTest.jsx
+++ src/components/testing/StoryTrayTest.jsx
@@ -1,5 +1,20 @@
 // <== IMPORTS ==>
+import StoryCarousel from "./StoryCarousel";
 
-const StoryTrayTest = () => {};
+const StoryTrayTest = () => {
+  // SLIDES COUNT FOR STORY CAROUSEL FOR TESTING
+  const SLIDE_COUNT = 20;
+  // STORY CAROUSEL API AUTO SCROLL OPTIONS
+  const STORY_CAROUSEL_OPTIONS = { slidesToScroll: 4 };
+  // SLIDES ARRAY TO PASS TO THE STORY CAROUSEL
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+  // COMPONENT'S RETURN
+  return (
+    <section className="mb-6">
+      {/* MAIN STORY CAROUSEL COMPONENT */}
+      <StoryCarousel slides={SLIDES} options={STORY_CAROUSEL_OPTIONS} />
+    </section>
+  );
+};
 
 export default StoryTrayTest;
