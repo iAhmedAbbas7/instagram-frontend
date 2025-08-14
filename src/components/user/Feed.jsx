0fecfe8714d3cf2a46bbc9610d4c25fb3cdfaa05
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-08-14 23:04:49 +0500

    Some Changes related to the UI in the Component

--- src/components/user/Feed.jsx
+++ src/components/user/Feed.jsx
@@ -5,7 +5,7 @@ import StoryTrayTest from "../testing/StoryTrayTest";
 const Feed = () => {
   return (
     // FEED MAIN WRAPPER
-    <div className="flex flex-col flex-1 items-center justify-center max-[768px]:pl-[0] max-[768px]:px-2 max-[768px]:pt-[75px] max-[768px]:pb-[35px] max-[1200px]:pl-[70px] pl-[250px]">
+    <div className="flex flex-col flex-1 items-center justify-center max-[768px]:pl-0 max-[768px]:mx-1 max-[768px]:pt-[75px] max-[768px]:pb-[35px] max-[1200px]:pl-[70px] pl-[250px]">
       <StoryTrayTest />
       <Posts />
     </div>
