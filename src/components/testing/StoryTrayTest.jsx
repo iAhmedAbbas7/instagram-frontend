2578e14c6bd44bf9e51d06731110bff052648acf
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-08-18 10:53:04 +0500

    Modified the Component with Real Stories Carousel Data

--- src/components/testing/StoryTrayTest.jsx
+++ src/components/testing/StoryTrayTest.jsx
@@ -2,17 +2,13 @@
 import StoryCarousel from "./StoryCarousel";
 
 const StoryTrayTest = () => {
-  // SLIDES COUNT FOR STORY CAROUSEL FOR TESTING
-  const SLIDE_COUNT = 20;
   // STORY CAROUSEL API AUTO SCROLL OPTIONS
   const STORY_CAROUSEL_OPTIONS = { slidesToScroll: 4 };
-  // SLIDES ARRAY TO PASS TO THE STORY CAROUSEL
-  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
   // COMPONENT'S RETURN
   return (
     <section className="mb-6">
       {/* MAIN STORY CAROUSEL COMPONENT */}
-      <StoryCarousel slides={SLIDES} options={STORY_CAROUSEL_OPTIONS} />
+      <StoryCarousel options={STORY_CAROUSEL_OPTIONS} />
     </section>
   );
 };
