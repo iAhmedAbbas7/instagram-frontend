25eb6975513271c44ce2c9c148828dc8cbabea44
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-09-02 11:58:31 +0500

    Introduced the Story Slice in Redux Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-// <= IMPORTS =>
+// <== IMPORTS ==>
 import authSlice from "./authSlice";
 import postSlice from "./postSlice";
 import chatSlice from "./chatSlice";
 import searchSlice from "./searchSlice";
+import storiesSlice from "./storySlice";
 import settingsSlice from "./settingsSlice";
 import storage from "redux-persist/lib/storage";
 import notificationSlice from "./notificationSlice";
@@ -17,27 +18,28 @@ import {
   REGISTER,
 } from "redux-persist";
 
-// <= REDUX-PERSIST =>
+// <== REDUX-PERSIST ==>
 const persistConfig = {
   key: "Instagram",
   version: 1,
   storage,
 };
 
-// <= APPLICATION-REDUCERS =>
+// <== APPLICATION-REDUCERS ==>
 const rootReducer = combineReducers({
   auth: authSlice,
   post: postSlice,
   chat: chatSlice,
   search: searchSlice,
+  stories: storiesSlice,
   settings: settingsSlice,
   notification: notificationSlice,
 });
 
-// <= REDUX-PERSIST =>
+// <== REDUX-PERSIST ==>
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// <= STORE WITH PERSISTENCE CONFIGURATION =>
+// <== STORE WITH PERSISTENCE CONFIGURATION ==>
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
